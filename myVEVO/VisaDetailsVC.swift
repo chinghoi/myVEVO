344868ac2648fd257c97cb75aeb2de202f8b5f62
chinghoi <56465334@qq.com>
2018-06-29 10:46:26 +0800

    2018.6.29

--- myVEVO/VisaDetailsVC.swift
+++ myVEVO/VisaDetailsVC.swift
@@ -9,8 +9,17 @@
 import UIKit
 import Eureka
 
-class VisaDetailsVC: UIViewController {
+class VisaDetailsVC: UITableViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
     }
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if section == 0 {
+            let header = view as? UITableViewHeaderFooterView
+            header?.textLabel?.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+
 }
--- myVEVO/VisaHolderEnquiryVC.swift
+++ myVEVO/VisaHolderEnquiryVC.swift
@@ -14,11 +14,17 @@ class VisaHolderEnquiryVC: FormViewController {
     var referenceNumber = ""
     var documentNumber = ""
     
+    var dataArray = [String]()
+    var filteredArray = [String]()
+    var shouldShowSearchResults = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let item = UIBarButtonItem(title: "back", style: .plain, target: self, action: nil)
         self.navigationItem.backBarButtonItem = item
+        
+        //读取国家数据
+        loadListOfCountries()
 
         
         form +++ Section("DOCUMENT TYPE")
@@ -36,7 +42,13 @@ class VisaHolderEnquiryVC: FormViewController {
             <<< DateRow(){
                 $0.title = "mm/dd/yyy"
                 $0.value = Date(timeIntervalSinceReferenceDate: 0)
-            }
+                $0.cell.tintColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+                print($0.value)
+                }.cellUpdate({ (cell, row) in
+                    //cell.textLabel?.textColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.9568627451, alpha: 1)
+                    cell.tintColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+
+                })
             +++ Section("DOCUMENT DETAILS")
             <<< TextRow(){
                 $0.placeholder = "Document Number"
@@ -44,9 +56,11 @@ class VisaHolderEnquiryVC: FormViewController {
                     if row2.value != nil {
                         self.documentNumber = row2.value!}}
             <<< PushRow<String>() {
-                $0.options = ["中国", "美国", "英国", "意大利", "德国", "叙利亚", "利比亚", "阿尔及利亚", "古巴", "埃及", "以色列", "越南", "泰国", "老挝", "加拿大", "巴西", "智力", "不丹", "沙特阿拉伯", "伊拉克", "南非", "蒙古", "韩国"]
-                $0.title = "Country Of Document"
-                $0.selectorTitle = "Choose an Country"
+                //加载国家数据
+                $0.options = dataArray
+                //$0.title = "Country Of Document"
+                //$0.selectorTitle = "Choose an Country"
+                $0.value = "Choose an Country"
                 }.onPresent { from, to in
                     to.dismissOnSelection = false
                     to.dismissOnChange = false
@@ -54,16 +68,65 @@ class VisaHolderEnquiryVC: FormViewController {
     }
     @IBAction func Submit(_ sender: UIButton) {
 
-        if (referenceNumber == "0049251846283" || referenceNumber == "EGOF2BQE82") && documentNumber == "EC5116279" {
-            let visaDetailsVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VisaDetails")
+        let visaDetailsVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VisaDetails")
+        
+        navigationController?.pushViewController(visaDetailsVC, animated: true)
+//        if (referenceNumber == "0049251846283" || referenceNumber == "EGOF2BQE82") && documentNumber == "EC5116279" {
+//            let visaDetailsVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VisaDetails")
+//            
+//            navigationController?.pushViewController(visaDetailsVC, animated: true)
+//        } else {
+//            let alertController = UIAlertController(title: "Incorrect PIN", message: "You have incorrectly entered your Pin three times. For security purposes, your details have been cleated. Please set a new PIN and enter your details to perform an enquiry.", preferredStyle: .alert)
+//            let alerAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+//            alertController.addAction(alerAction)
+//            self.present(alertController, animated: true, completion: nil)
+//        }
+    }
+    func loadListOfCountries() {
+        // 得到国家列表文件的路径
+        let pathToFile = Bundle.main.path(forResource: "countries", ofType: "txt")
+        
+        if let path = pathToFile {
+            // 得到文件内容，加载到一个字符串中
+            //(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)!
+            let countriesString = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
             
-            navigationController?.pushViewController(visaDetailsVC, animated: true)
-        } else {
-            let alertController = UIAlertController(title: "Incorrect PIN", message: "You have incorrectly entered your Pin three times. For security purposes, your details have been cleated. Please set a new PIN and enter your details to perform an enquiry.", preferredStyle: .alert)
-            let alerAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-            alertController.addAction(alerAction)
-            self.present(alertController, animated: true, completion: nil)
+            // 通过换行符分割，得到国家列表，存到数组中
+            dataArray = countriesString.components(separatedBy: "\n")//countriesString.componentsSeparatedByString("\n")
+            
+            // 刷新tableView
+            //tblSearchResults.reloadData()
         }
     }
+    // 自定义value类型是Bool的Cell
+    // Cell是使用 .xib 定义的，所以我们可以直接设置outlets
+    public class CustomCell: Cell<Bool>, CellType {
+        @IBOutlet weak var switchControl: UISwitch!
+        @IBOutlet weak var label: UILabel!
+        
+        public override func setup() {
+            super.setup()
+            switchControl.addTarget(self, action: #selector(CustomCell.switchValueChanged), for: .valueChanged)
+        }
+        
+        @objc func switchValueChanged(){
+            row.value = switchControl.isOn
+            row.updateCell() // Re-draws the cell which calls 'update' bellow
+        }
+        
+        public override func update() {
+            super.update()
+            backgroundColor = (row.value ?? false) ? .white : .black
+        }
+    }
+    
+    // 自定义的Row，拥有CustomCell和对应的value
+    public final class CustomRow: Row<CustomCell>, RowType {
+        required public init(tag: String?) {
+            super.init(tag: tag)
+            // 我们把对应CustomCell的 .xib 加载到cellProvidor
+            cellProvider = CellProvider<CustomCell>(nibName: "DateCell")
+        }
+    }    
 }
-
+//class DateCell: ListCheckCell<Date> {}
